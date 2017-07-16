ab6673d92a50ea5540bc78d4c967b9cb13f42d71
magictime <magictime@users.noreply.github.com>
2017-07-15 23:31:54 -0700

    Update gen_code.php
    add html tag

--- gen_code.php
+++ gen_code.php
@@ -1,4 +1,9 @@
- <?php
+ <!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
 $story_num = "3";
 $title = "W3Schools.com";
 $article_source = 5;
@@ -8,3 +13,6 @@ echo "<h2>$story_num</h2>";
 echo "Learn TCM at $title<br>";
 echo "Translated from $article_source" ;
 ?> 
+ 
+ </body>
+</html>
