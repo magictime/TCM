cb4ecde48081a23076ea9e05d9a0c7230afffd23
magictime <magictime@users.noreply.github.com>
2017-07-15 23:26:09 -0700

    Create gen_code.php
    script to generate code for each story display

---
+++ gen_code.php
@@ -0,0 +1,10 @@
+ <?php
+$story_num = "3";
+$title = "W3Schools.com";
+$article_source = 5;
+$content = "you are brillient";
+
+echo "<h2>$story_num</h2>";
+echo "Learn TCM at $title<br>";
+echo "Translated from $article_source" ;
+?> 
